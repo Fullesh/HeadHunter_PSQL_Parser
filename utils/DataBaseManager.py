af905fca26f53267afdc41915e1ab1aa4af59d9e
Danila Belov <danilabelov.adm@yandex.ru>
2024-01-26 18:49:35 +0300

    Added DataBaseManager class

---
+++ utils/DataBaseManager.py
@@ -0,0 +1,19 @@
+class DBManager:
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    def get_all_vacancies(self):
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self):
+        pass
