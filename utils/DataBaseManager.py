f00f800e2ef6aa0da066e06d90ad3943beadd8f4
Danila Belov <danilabelov.adm@yandex.ru>
2024-02-19 20:15:13 +0300

    Bug fixes in Database manager class

--- utils/DataBaseManager.py
+++ utils/DataBaseManager.py
@@ -16,14 +16,51 @@ class DBManager:
                            'password': '1'}
 
     def __repr__(self):
-        pass
+        return f'Параметры подключения к БД: {self.new_params} \n' \
+               f'Существует ли БД: {self.check_databases()} \n' \
+               f'Название БД: headhunter_database \n' \
+               f'Пользователь БД: postgres'
 
     def open_con(self, connection_params):
+        """
+        Открывает подключение к БД
+        :param connection_params: параметры для подключения
+        :return: Открытое подключение
+        """
         self.connection = psycopg2.connect(**connection_params)
         self.connection.autocommit = True
         return self.connection
 
+    def check_databases(self):
+        """
+        Функция для проверки существует ли БД
+        :return: True/False
+        """
+        try:
+            self.open_con(self.new_params)
+            print('Успешное подключение к БД headhunter_database')
+            return True
+        except OperationalError:
+            print('БД уже удалена либо ещё не существует, пропускаем...')
+
+    def total_drop(self):
+        """
+        Полностью удаляет БД и таблцы в ней
+        :return: None
+        """
+        try:
+            self.open_con(self.new_params)
+            with self.connection.cursor() as cur:
+                cur.execute('DROP DATABASE headhunter_database with (FORCE)')
+        except OperationalError:
+            print('База данных уже удалена или ещё не существует, пропускаем...')
+        self.delete_tables()
+
     def create_database(self):
+        """
+        Функция создает БД если её ещё не существует
+        :return:
+        """
         self.open_con(self.conn_params)
         with self.connection.cursor() as cur:
             try:
@@ -33,6 +70,10 @@ class DBManager:
         self.connection.close()
 
     def initialisation_of_tables(self):
+        """
+        Функция создаёт таблцы с работниками и вакансиями
+        :return: None
+        """
         self.open_con(self.new_params)
         with self.connection.cursor() as cur:
             try:
@@ -42,19 +83,25 @@ class DBManager:
                             employer_url varchar(100),
                             open_vacancies INT )""")
                 cur.execute("""create table vacancies (
-                            id INT PRIMARY KEY,
+                            id INT,
                             company varchar(100),
                             employer_url varchar(100),
                             salary_min int,
                             salary_max int,
                             city varchar(100),
-                            vacancy_id int,
+                            vacancy_id int PRIMARY KEY,
                             vacancy_name varchar(100),
                             vacancy_url varchar(100))""")
             except DuplicateTable:
                 print('Одна или обе таблицы существуют, пропускаем команду')
 
     def append_employers_and_vacancies(self, employers, vacancies):
+        """
+        Функция добавляет в БД данные с работодателями и вакансиями
+        :param employers: Словарь с данными о работодателях
+        :param vacancies: Словарь с данными о вакансиях
+        :return: None
+        """
         self.open_con(self.new_params)
         with self.connection.cursor() as cur:
             try:
@@ -89,6 +136,10 @@ class DBManager:
             pass
 
     def get_all_vacancies(self):
+        """
+        Функция выводит информацию о всех вакансиях в БД
+        :return:
+        """
         self.open_con(self.new_params)
         with self.connection.cursor() as cur:
             cur.execute('SELECT * FROM VACANCIES')
@@ -102,10 +153,17 @@ class DBManager:
     def get_vacancies_with_keyword(self, keyword):
         pass
 
-    def clear_tables(self):
-        self.open_con(self.new_params)
-        with self.connection.cursor() as cur:
-            cur.execute('TRUNCATE TABLE employers')
-            cur.execute('TRUNCATE TABLE vacancies')
-        print('Успешно очистили таблицу employers')
-        print('Успешно очистили таблицу vacancies')
+    def delete_tables(self):
+        """
+        Функция удаляет таблицы с работодателями и вакансиями
+        :return: None
+        """
+        try:
+            self.open_con(self.new_params)
+            with self.connection.cursor() as cur:
+                cur.execute('DROP TABLE employers')
+                cur.execute('DROP TABLE vacancies')
+            print('Успешно удалили таблицу employers')
+            print('Успешно удалили таблицу vacancies')
+        except UndefinedTable:
+            print('Таблиц уже не существует')
